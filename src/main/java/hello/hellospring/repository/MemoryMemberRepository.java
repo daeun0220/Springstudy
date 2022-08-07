b76d61a9df6ef16a9c1a9bca1b72f8896bb00a21
daeun0220 <daeunkim1284@gmail.com>
2022-08-07 13:26:40 +0900

    member ex

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,34 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; // type이 long 이라서 L 붙임
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));  // null값을 가질 때 Optional로 묶어서 반환한다
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
